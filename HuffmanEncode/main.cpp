50fecf6a022a7ca708f13aaaa16f9a9af41a8f45
Piotr Gorski <slmblck@gmail.com>
2014-05-04 00:17:43 -0500

    It's Done!
    Here is the completed project. There is still a lot of debug code in
    there. I can remove that if you guys really want.

--- HuffmanEncode/main.cpp
+++ HuffmanEncode/main.cpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <string>
 #include <iterator>
+#include <algorithm>
+#include <stdio.h>
+#include <sstream>
 
 using namespace std;
 
 typedef struct node node;
+
 
 struct node{
     int frequency;
@@ -16,8 +20,95 @@ struct node{
     node *right;
 };
 
+struct tableNode{
+    string val;
+    string code;
+};
+
+typedef struct tableNode tableNode;
+
+vector<tableNode*> inTableV;
+vector<tableNode*> outTableV;
+
+string int_array_to_string(int int_array[], int size_of_array){
+    std::stringstream oss("");
+    for (int temp = 0; temp < size_of_array; temp++)
+        oss << int_array[temp];
+    return oss.str();
+}
+
 bool listCmp (node *i,node *j) {
     return (i->frequency > j->frequency);
+}
+
+tableNode *createTableNode(int arr[], int n, string s){
+    tableNode *newTab = new tableNode;
+    stringstream oss;
+    for(int i = 0; i < n; ++i){
+       oss << arr[i];
+    }
+    newTab->code = oss.str();
+    newTab->val = s;
+    return newTab;
+}
+
+void printArr(int arr[], int n){
+    for (int i = 0; i < n; ++i){
+        cout << arr[i];
+    }
+    cout << endl;
+}
+
+void printCodes(node *root, int arr[], int top)
+{
+    if (root->left) {
+        arr[top] = 0;
+        printCodes(root->left, arr, top + 1);
+    }
+
+    if (root->right) {
+        arr[top] = 1;
+        printCodes(root->right, arr, top + 1);
+    }
+
+    if(root->right == NULL && root->left == NULL) {
+        cout << root->val << ": ";
+        printArr(arr, top);
+    }
+}
+
+void printCodesV(node *root, int arr[], int top)
+{
+    if (root->left) {
+        arr[top] = 0;
+        printCodesV(root->left, arr, top + 1);
+    }
+
+    if (root->right) {
+        arr[top] = 1;
+        printCodesV(root->right, arr, top + 1);
+    }
+
+    if(root->right == NULL && root->left == NULL) {
+        inTableV.push_back(createTableNode(arr, top, root->val));
+    }
+}
+
+void printCodesVO(node *root, int arr[], int top)
+{
+    if (root->left) {
+        arr[top] = 0;
+        printCodesVO(root->left, arr, top + 1);
+    }
+
+    if (root->right) {
+        arr[top] = 1;
+        printCodesVO(root->right, arr, top + 1);
+    }
+
+    if(root->right == NULL && root->left == NULL) {
+        outTableV.push_back(createTableNode(arr, top, root->val));
+    }
 }
 
 int substring_compare(string x, string y){
@@ -69,10 +160,18 @@ int main(int argc, char* argv[])
     char *testFile = argv[2];
     int breakUpFactor = atoi(argv[3]);
     int numInputs = 0;
+    int *table;
+    int *outTable;
+    long long int inLength = 0;
+    long long int outLength = 0;
+    long long int inEncrypted = 0;
+    long long int outEncrypted = 0;
     vector<string> inputVectors;
     vector<string> outputVectors;
     vector<string> inputVectorsBroken;
     vector<string> outputVectorsBroken;
+    vector<string> inputVectorsBrokenCopy;
+    vector<string> outputVectorsBrokenCopy;
     vector<string> inputVectorsUnique;
     vector<float> numInputVectorsUnique;
     vector<string> outputVectorsUnique;
@@ -80,6 +179,7 @@ int main(int argc, char* argv[])
     vector<float> probInput;
     vector<float> probOutput;
     vector<node*> nodeList;
+    vector<node*> outList;
     float numUniqueIn = 0;
     float numUniqueOut = 0;
 
@@ -153,6 +253,8 @@ int main(int argc, char* argv[])
                 string outputVec = inputNOutput.substr(endInput+1);
                 size_t outSize = outputVec.size();
 
+                //inLength = inLength + inputVec.length();
+                //outLength = outLength + outputVec.length();
                 //Add the input and output to the vector
                 inputVectors.push_back(inputVec);
                 outputVectors.push_back(outputVec);
@@ -216,6 +318,8 @@ int main(int argc, char* argv[])
     }
     cout << endl;
 
+    inputVectorsBrokenCopy = inputVectorsBroken;
+
     //Now that we have the input and output vectors all broken up
     //Count them up to see if we can compress any
     //This looks bad but has okay runtime
@@ -236,6 +340,8 @@ int main(int argc, char* argv[])
         //temp.clear();
         numInputVectorsUnique.push_back(count);
     }
+
+    outputVectorsBrokenCopy = outputVectorsBroken;
 
     for(int i = 0; (unsigned)i < outputVectorsBroken.size(); i++){
         string temp = outputVectorsBroken[i];
@@ -292,6 +398,10 @@ int main(int argc, char* argv[])
         nodeList.push_back(createLeaf(inputVectorsUnique.at(i), numInputVectorsUnique.at(i)));
     }
 
+    for(int i = 0; (unsigned)i < outputVectorsUnique.size(); i++){
+        outList.push_back(createLeaf(outputVectorsUnique.at(i), numOutputVectorsUnique.at(i)));
+    }
+
     cout << "nodeList test" << endl;
     for(int i = 0; (unsigned)i < nodeList.size(); i++){
         node *temp = nodeList.at(i);
@@ -301,6 +411,8 @@ int main(int argc, char* argv[])
 
     //Now sort
     sort(nodeList.begin(), nodeList.end(), listCmp);
+
+    sort(outList.begin(), outList.end(), listCmp);
 
     cout << "nodeList sort test" << endl;
     for(int i = 0; (unsigned)i < nodeList.size(); i++){
@@ -332,6 +444,27 @@ int main(int argc, char* argv[])
         cout << endl;
     }
 
+    while(outList.size() > 1){
+        node *temp = outList.at(outList.size() - 1);
+        node *temp1 = outList.at(outList.size() - 2);
+        outList.push_back(buildUp(temp, temp1));
+        //node *p = nodeList.at(nodeList.size() -1);
+        //cout << p->frequency << endl;
+        //if(nodeList.size() > 2){
+        outList.erase(outList.end() - 2);
+        outList.erase(outList.end() - 2);
+        //} else {
+            //nodeList.erase(nodeList.end() - 1);
+        //}
+        sort(outList.begin(), outList.end(), listCmp);
+        for(int i = 0; (unsigned)i < outList.size(); i++){
+            node *temp = outList.at(i);
+            int num = temp->frequency;
+            cout << num << endl;
+        }
+        cout << endl;
+    }
+
     cout << "nodeList build test" << endl;
     for(int i = 0; (unsigned)i < nodeList.size(); i++){
         node *temp = nodeList.at(i);
@@ -340,7 +473,108 @@ int main(int argc, char* argv[])
         //cout << temp->left->frequency << endl;
     }
 
-    cout << "Got to the end" << endl;
+    cout << "outList build test" << endl;
+    for(int i = 0; (unsigned)i < outList.size(); i++){
+        node *temp = outList.at(i);
+        int num = temp->frequency;
+        cout << num << endl;
+        //cout << temp->left->frequency << endl;
+    }
+
+    /*+---------------------------------------------------------------+
+      | Here is where we will try to encode the string using the tree |
+      +---------------------------------------------------------------+*/
+
+    /* Fist thing first, build the table for encoding */
+    table = (int*)malloc(sizeof(int)*inputVectorsUnique.size());
+    outTable = (int*)malloc(sizeof(int)*outputVectorsUnique.size());
+
+    //testtable = (int*)malloc(sizeof(int)*inputVectorsUnique.size());
+
+
+
+    printCodesV(nodeList[0], table, 0);
+    printCodesVO(outList[0], outTable, 0);
+
+    //printCodesV(nodeList[0], testtable, 0);
+
+    cout << "Input Encoding Table:" << endl;
+    for(int i = 0; (unsigned)i < inTableV.size(); i++){
+        cout << inTableV[i]->val << " : ";
+        cout << inTableV[i]->code << endl;
+    }
+
+    cout << "Output Encoding Table:" << endl;
+    cout << endl;
+
+    for(int i = 0; (unsigned)i < outTableV.size(); i++){
+        cout << outTableV[i]->val << " : ";
+        cout << outTableV[i]->code << endl;
+    }
+
+    cout << endl;
+
+    cout << "Encrypted input: " << endl;
+
+    //encode input
+    for(int i = 0; (unsigned)i < inputVectorsBrokenCopy.size(); i++){
+        string temp = inputVectorsBrokenCopy[i];
+        //inputVectorsUnique.push_back(temp);
+        int count = 0;
+        //cout << "Got here " << i << endl;
+        for(int j = 0; (unsigned)j < inTableV.size(); j++){
+            string stemp = inTableV[j]->val;
+            size_t found;
+            if((found = temp.find(stemp)) != string::npos){
+                //cout << temp << "Equivalent to " << stemp << endl;
+                inLength = inLength + inTableV[j]->val.length();
+                inEncrypted = inEncrypted + inTableV[j]->code.length();
+                cout << inTableV[j]->code << endl;
+                count++;
+                //inputVectorsBroken.erase(inputVectorsBroken.begin() + j);
+            }
+        }
+        //temp.clear();
+        //numInputVectorsUnique.push_back(count);
+    }
+
+    cout << endl;
+
+    cout << "Encrypted output: " << endl;
+
+    //encode input
+    for(int i = 0; (unsigned)i < outputVectorsBrokenCopy.size(); i++){
+        string temp = outputVectorsBrokenCopy[i];
+        //inputVectorsUnique.push_back(temp);
+        int count = 0;
+        //cout << "Got here " << i << endl;
+        for(int j = 0; (unsigned)j < outTableV.size(); j++){
+            string stemp = outTableV[j]->val;
+            size_t found;
+            if((found = temp.find(stemp)) != string::npos){
+                //cout << temp << "Equivalent to " << stemp << endl;
+                outLength = outLength + outTableV[j]->val.length();
+                outEncrypted = outEncrypted + outTableV[j]->code.length();
+                cout << outTableV[j]->code << endl;
+                count++;
+                //inputVectorsBroken.erase(inputVectorsBroken.begin() + j);
+            }
+        }
+        //temp.clear();
+        //numInputVectorsUnique.push_back(count);
+    }
+
+    cout << endl;
+
+    cout << "Here are the stats: " << endl;
+    cout << "Input bits before encryption: " << inLength << endl;
+    cout << "Input bits after encryption: " << inEncrypted << endl;
+    cout << "Output bits before encryption: " << outLength << endl;
+    cout << "Output bits after encryption: " << outEncrypted << endl;
+
+    cout << endl;
+
+    //cout << "Got to the end" << endl;
 
     return 0;
 }
